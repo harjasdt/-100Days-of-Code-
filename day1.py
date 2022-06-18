8e55133df9b10737fad2c1ac9afc7272a2345bee
Harjaspreet singh <singh.harjas2002@gmail.com>
2022-06-18 11:09:39 +0530

    day1

---
+++ day1.py
@@ -0,0 +1,6 @@
+print("Welcome to the band Name Generator.")
+print("What's name of the city you grew up in?")
+namel=input()
+print("What's your pet's name?")
+pet=input()
+print("Your band name could be " + namel +" "+pet)
