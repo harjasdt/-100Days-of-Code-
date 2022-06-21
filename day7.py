2fe46f229e733aa66ba706831e53508521e92130
Harjaspreet singh <singh.harjas2002@gmail.com>
2022-06-21 15:31:57 +0530

    Hangman Game

---
+++ day7.py
@@ -0,0 +1,163 @@
+import random
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+logo = '''
+ _
+| |
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |
+                   |___/'''
+print(logo)
+life=6
+gameend=False
+words=[
+'cockiness',
+'croquet',
+'crypt',
+'curacao',
+'cycle',
+'daiquiri',
+'dirndl',
+'disavow',
+'dizzying',
+'duplex',
+'dwarves',
+'embezzle',
+'equip',
+'espionage',
+'euouae',
+'exodus',
+'faking',
+'fishhook',
+'fixable',
+'fjord',
+'flapjack',
+'flopping',
+'fluffiness',
+'flyby',
+'foxglove',
+'frazzled',
+'frizzled',
+'fuchsia',
+'funny',
+'gabby',
+'galaxy',
+'galvanize',
+'gazebo',
+'giaour',
+'gizmo',
+'glowworm',
+'glyph',
+'gnarly',
+'gnostic',
+'gossip',
+'grogginess',
+'haiku',
+'haphazard',
+'hyphen',
+'iatrogenic',
+'icebox',
+'injury',
+'ivory',
+'ivy',
+'jackpot',
+'jaundice',
+'jawbreaker',
+'jaywalk',
+'jazziest',
+'jazzy',
+'jelly',
+'jigsaw',
+'jinx',
+'jiujitsu',
+'jockey',
+'jogging',
+'joking',
+'jovial',
+'joyful',
+]
+word=random.choice(words)
+print(word)
+ans=[]
+user=[] #list to store entered letters
+for i in word:
+    ans.append('_')
+while not gameend:
+    guess=input("Enter your guess: ").lower()
+    ind=0
+    for i in word:
+        if guess==i:
+            ans[ind]=i
+        ind+=1
+    print(ans)
+    if guess not in ans:
+        if guess in user:
+            print(f"You have already Tried this letter!! \n Guessed letter: {guess}")
+        else:
+            print(stages[life])
+            life-=1
+            print(f"{guess} is not the word :( \n you loose a life")
+    user.append(guess)
+    if life==-1:
+        print("You loose")
+        gameend=True
+    if '_' not in ans:
+        print("you win")
+        gameend=True
