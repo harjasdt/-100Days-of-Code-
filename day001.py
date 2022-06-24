02c9c2be042e9e363a934462b2ed884ae9ab3892
Harjaspreet <68768529+harjasDot@users.noreply.github.com>
2022-06-24 18:25:48 +0530

    Update day001.py

--- day001.py
+++ day001.py
@@ -1,5 +1,5 @@
 print("Welcome to the band Name Generator.")
-print("What's name of the city you grew up in?")
+print("What's name of the city you grew up  in?")
 namel=input()
 print("What's your pet's name?")
 pet=input()
