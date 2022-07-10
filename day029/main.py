c2994c98976a610b044567bfb112c6fcddc13b0c
Harjaspreet singh <singh.harjas2002@gmail.com>
2022-07-10 14:17:08 +0530

    tkinter password manager

---
+++ day029/main.py
@@ -0,0 +1,69 @@
+from tkinter import *
+from tkinter import messagebox
+import random
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def gen():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+    ans = []
+    for i in range(0, 9):
+        ans.append(random.choice(letters))
+    for i in range(0, 4):
+        ans.append(random.choice(symbols))
+    for i in range(0, 6):
+        ans.append(random.choice(numbers))
+    random.shuffle(ans)
+    passw=""
+    passw="".join(ans)
+    user3.delete(0, "end")
+    user3.insert(0,passw)
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def addition():
+    if user1.get()=="" or user2.get()=="" or user3.get()=="":
+        messagebox.showerror(title="OOPS!!!", message="DO NOT LEAVE ANY FIELD EMPTY")
+    else:
+        flag=messagebox.askokcancel(title="Success", message="DATA ADDED")
+        if flag:
+            with open("data.txt", "a") as data:
+                data.write(f'{user1.get()} | {user2.get()} | {user3.get()}\n')
+                user1.delete(0, "end")
+                user3.delete(0, "end")
+# ---------------------------- UI SETUP ------------------------------- #
+window=Tk()
+window.title("Password Manager")
+window.config(pady=20, padx=20)
+canvas=Canvas(height=200, width=200)
+pic=PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=pic)
+canvas.grid(row=0, column=1)
+
+l1=Label(text="Website")
+l1.grid(row=1, column=0)
+
+user1=Entry(width=35)
+user1.grid(row=1, column=1, columnspan=2)
+user1.focus()
+
+l2=Label(text="Email/username")
+l2.grid(row=2, column=0)
+
+user2=Entry(width=35)
+user2.grid(row=2, column=1, columnspan=2)
+user2.insert(0, "s@mail")
+
+l3=Label(text="Password")
+l3.grid(row=3, column=0)
+
+user3=Entry(width=21)
+user3.grid(row=3,column=1)
+
+b1=Button(text="Generate Password", command=gen)
+b1.grid(row=3, column=2)
+
+b2=Button(text="Add", width=36, command=addition)
+b2.grid(row=4, column=1, columnspan=2)
+window.mainloop()
